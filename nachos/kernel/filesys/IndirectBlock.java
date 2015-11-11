38f1d2cc2d57841d4add278515ffd9320b1ac1ad
Maedeh Safari <maedeh.safari@gmail.com>
2015-11-11 02:06:11 -0500

    feat(Indirect & DoublyDirect): just added the files. NOT STABLE DID NOT TEST

--- nachos/kernel/filesys/IndirectBlock.java
+++ nachos/kernel/filesys/IndirectBlock.java
@@ -1,51 +1,15 @@
-// FileHeader.jave
-//	Routines for managing the disk file header (in UNIX, this
-//	would be called the i-node).
-//
-// Copyright (c) 1992-1993 The Regents of the University of California.
-// Copyright (c) 1998 Rice University.
-// Copyright (c) 2003 State University of New York at Stony Brook.
-// All rights reserved.  See the COPYRIGHT file for copyright notice and 
-// limitation of liability and disclaimer of warranty provisions.
-
+/**
+ * 
+ */
 package nachos.kernel.filesys;
 
 import nachos.Debug;
 
 /**
- * This class defines the Nachos "file header" (in UNIX terms,  
- * the "i-node"), describing where on disk to find all of the data
- * in the file.  The file header is organized as a simple table of
- * pointers to data blocks. 
+ * @author maedeh
  *
- * The file header data structure can be stored in memory or on disk.
- * When it is on disk, it is stored in a single sector -- this means
- * that we assume the size of this data structure to be the same
- * as one disk sector.  Without indirect addressing, this
- * limits the maximum file length to just under 4K bytes.
- *
- * The file header is used to locate where on disk the 
- * file's data is stored.  We implement this as a fixed size
- * table of pointers -- each entry in the table points to the 
- * disk sector containing that portion of the file data
- * (in other words, there are no indirect or doubly indirect 
- * blocks). The table size is chosen so that the file header
- * will be just big enough to fit in one disk sector, 
- *
- * Unlike in a real system, we do not keep track of file permissions, 
- * ownership, last modification date, etc., in the file header. 
- *
- * A file header can be initialized in two ways:
- *   for a new file, by modifying the in-memory data structure
- *      to point to the newly allocated data blocks;
- *   for a file already on disk, by reading the file header from disk.
- *
- * @author Thomas Anderson (UC Berkeley), original C++ version
- * @author Peter Druschel (Rice University), Java translation
- * @author Eugene W. Stark (Stony Brook University)
  */
-class FileHeader {
-
+public class IndirectBlock {
     /** Number of pointers to data blocks stored in a file header. */
     private final int NumDirect;
 
@@ -72,16 +36,16 @@ class FileHeader {
      * 
      * @param filesystem  The underlying filesystem in which the file header resides.
      */
-    FileHeader(FileSystemReal filesystem) {
+    public IndirectBlock(FileSystemReal filesystem) {
 	this.filesystem = filesystem;
 	diskSectorSize = filesystem.diskSectorSize;
 	NumDirect = ((diskSectorSize - 2 * 4) / 4);
 	MaxFileSize = (NumDirect * diskSectorSize);
 
-	dataSectors = new int[NumDirect];
+	dataSectors = new int[NumDirect + 2];
 	// Safest to fill the table with garbage sector numbers,
 	// so that we error out quickly if we forget to initialize it properly.
-	for(int i = 0; i < NumDirect - 2; i++)
+	for(int i = 0; i < NumDirect; i++)
 	    dataSectors[i] = -1;
 	
     }
@@ -128,16 +92,13 @@ class FileHeader {
      * @param freeMap is the bit map of free disk sectors.
      * @param fileSize is size of the new file.
      */
-    boolean allocate(BitMap freeMap, int fileSize) {
-	if(fileSize > MaxFileSize)
+    boolean allocate(BitMap freeMap, int numSectors) {
+	if(numSectors * diskSectorSize > MaxFileSize)
 	    return false;		// file too large
-	numBytes = fileSize;
-	numSectors  = fileSize / diskSectorSize;
-	if (fileSize % diskSectorSize != 0) numSectors++;
 
 	if (freeMap.numClear() < numSectors || NumDirect < numSectors)
 	    return false;		// not enough space
-
+	
 	for (int i = 0; i < numSectors; i++){
 	    dataSectors[i] = freeMap.find(); 
 	}
@@ -145,26 +106,6 @@ class FileHeader {
 	return true;
     }
     
-    
-    /**
-     * Extend the file 
-     * @param freeMap is the bit map of free disk sectors.
-     * @param fileSize is size of the new file.
-     */
-    boolean extend(BitMap freeMap, int fileSize) {
-	if(fileSize > MaxFileSize)
-	    return false;		// file too large
-	numBytes = fileSize;
-	numSectors  = fileSize / diskSectorSize;
-	if (fileSize % diskSectorSize != 0) numSectors++;
-
-	if (freeMap.numClear() < numSectors || NumDirect < numSectors)
-	    return false;		// not enough space
-
-	for (int i = 0; i < numSectors; i++)
-	    dataSectors[i] = freeMap.find();
-	return true;
-    }
 
     /**
      * De-allocate all the space allocated for data blocks for this file.
@@ -213,39 +154,5 @@ class FileHeader {
 	return(dataSectors[offset / diskSectorSize]);
     }
 
-    /**
-     * Retrieve the number of bytes in the file.
-     *
-     * @return the number of bytes in the file.
-     */
-    int fileLength() {
-	return numBytes;
-    }
-
-    /**
-     * 	Print the contents of the file header, and the contents of all
-     *	the data blocks pointed to by the file header.
-     */
-    void print() {
-	int i, j, k;
-	byte data[] = new byte[diskSectorSize];
-
-	System.out.print("FileHeader contents.  File size: " + numBytes
-		+ ".,  File blocks: ");
-	for (i = 0; i < numSectors; i++)
-	    System.out.print(dataSectors[i] + " ");
-
-	System.out.println("\nFile contents:");
-	for (i = k = 0; i < numSectors; i++) {
-	    filesystem.readSector(dataSectors[i], data, 0);
-	    for (j = 0; (j < diskSectorSize) && (k < numBytes); j++, k++) {
-		if ('\040' <= data[j] && data[j] <= '\176')   // isprint(data[j])
-		    System.out.print((char)data[j]);
-		else
-		    System.out.print("\\" + Integer.toHexString(data[j] & 0xff));
-	    }
-	    System.out.println();
-	}
-    }
 
 }

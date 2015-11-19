9baa58ae35d0e0127ad640d59a2f1d836db5e4ac
Yakov Gaberman <yakov.gaberman@stonybrook.edu>
2015-11-19 02:26:49 -0500

    feat(extend3): file used to test extending the directory

---
+++ test/extend3.c
@@ -0,0 +1,23 @@
+#include "syscall.h"
+
+int main()
+{
+  Create("create0");
+  Create("create1");
+  Create("create2");
+  Create("create3");
+  Create("create4");
+  Create("create5");
+  Create("create6");
+  Create("create7");
+  Create("create8");
+  Create("create9");
+  Create("create10");
+  Create("create11");
+  Create("create12");
+  Create("create13");
+  Create("create14");
+  Create("create15");
+  Create("create16");
+  Create("create17");
+}
